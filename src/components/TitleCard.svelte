81a80bb19bf8290702b4527bab632ff64f3a9027
Fuzail22 <fuzailhaariss@gmail.com>
2024-05-08 18:40:29 +0530

    titleCardWdithincreased

--- src/components/TitleCard.svelte
+++ src/components/TitleCard.svelte
@@ -9,13 +9,13 @@
 </script>
 
 <div
-	class="sm:w-[500px] font-medium mb-12 md:mb-24"
+	class="sm:w-[540px] font-medium mb-12 md:mb-24"
 	style="text-align:{centerText ? 'center' : 'left'};"
 >
 	<div class="text-2xl" style="color:var({titleColor},white);">
 		{title}
 	</div>
-	<div class="text-3xl sm:text-5xl text-wrap" style="color:var({descriptionColor},white)">
+	<div class="text-3xl sm:text-5xl" style="color:var({descriptionColor},white)">
 		{description}
 	</div>
 </div>
